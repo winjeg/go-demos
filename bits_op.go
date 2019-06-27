437e7958d61d8c7c11e29dfe27c314e084bcf21f
winjeg <winjeg@qq.com>
2019-06-27 11:11:22 +0800

    add bit op or letcode

---
+++ bits_op.go
@@ -0,0 +1,38 @@
+/// @Author winjeg,  winjeg@qq.com
+/// All rights reserved to winjeg
+
+package main
+
+import "fmt"
+
+func reverseBits(num uint32) uint32 {
+	t := num
+	result := uint32(0)
+	for i := uint32(0); i < 32; i++ {
+		fmt.Printf("%32b\n", num)
+		x := num << uint32(31)
+		x >>= i
+		fmt.Printf("%32b\n", x)
+		result &= x
+		num = t >> (i + 1)
+	}
+	return result
+}
+
+func hammingDistance(x int, y int) int {
+	r := x ^ y
+	count := 0
+	for r != 0 {
+		r &= r - 1
+		count++
+	}
+	return count
+}
+func hammingWeight(num uint32) int {
+	count := 0
+	for num != 0 {
+		num &= num - 1
+		count++
+	}
+	return count
+}
--- main.go
+++ main.go
@@ -1,8 +1,5 @@
 package main
 
-import "github.com/winjeg/demo/ui"
-
 func main() {
-	ui.FyneUi()
 }
 
